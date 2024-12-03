4541338da054f67210305dacb4f9c6b2ad84c67e
David Donovan <daviddenovannn@gmail.com>
2024-12-03 14:56:41 +0700

    revisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -6,9 +6,8 @@ console.log(add(1, 2));
 const substract = (a, b) => a - b;
 console.log(substract(5, 2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(5, 2));
-
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(5, 2));
 
 const divide = (a, b) => a / b;
 console.log(divide(2, 2));
